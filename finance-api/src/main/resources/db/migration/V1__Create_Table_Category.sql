5765f8b3e5b5fcbb7cb961bb79e92247074d01c2
Gustavo Moraes <gustavo.moliveira@bandtec.com.br>
2022-10-03 12:02:07 -0300

    flyway dependency fixed for postgresql db

--- finance-api/src/main/resources/db/migration/V1__Create_Table_Category.sql
+++ finance-api/src/main/resources/db/migration/V1__Create_Table_Category.sql
@@ -1,5 +1,5 @@
-CREATE TABLE IF NOT EXISTS `category` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `description` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
+CREATE TABLE IF NOT EXISTS category (
+  id bigint NOT NULL,
+  description varchar(255) NOT NULL,
+  PRIMARY KEY (id)
 );
