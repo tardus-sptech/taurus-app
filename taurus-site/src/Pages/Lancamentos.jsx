e4a83abad8cb4bb13ff56d9639104e1f52643303
Victor Linhati <VICTOR.LINHATI@bandtec.com.br>
2022-11-20 00:22:07 -0300

    Lançamento não estilizado

--- taurus-site/src/Pages/Lancamentos.jsx
+++ taurus-site/src/Pages/Lancamentos.jsx
@@ -1,4 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
+
+import Dialog from "@mui/material/Dialog";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
 
 import Pontos from "../assets/pontos.png";
 import Faca from "../assets/faca.png";
@@ -6,8 +10,24 @@ import Casa from "../assets/casa.png";
 import Lista from "../assets/list.png";
 import NavBar from "../Components/NavBar";
 import Footer from "../Components/Footer";
+import Teste from "./NovoLancamento";
+import SimpleDialogDemo from "./teste";
 
-function Lancamentos() {
+
+function Lancamentos(props) {
+  const handleOpen = () => setOpen(true);
+  const [open, setOpen] = React.useState(false);
+  const [selectedValue, setSelectedValue] = React.useState();
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = (value) => {
+    setOpen(false);
+    setSelectedValue(value);
+  };
+
   return (
     <>
       <NavBar />
@@ -15,7 +35,12 @@ function Lancamentos() {
         <span className="alinhamentoBox">
           <div className="tamanhoBox">
             <div className="titulos">
-              <h3>Lançamentos</h3>
+              <h3>
+                Lançamentos
+                <Button onClick={handleOpen} className="btnLancamento">
+                  +
+                </Button>
+              </h3>
               <h3>Novembro</h3>
               <h3>Contas</h3>
             </div>
@@ -74,6 +99,17 @@ function Lancamentos() {
               <div className="custo">
                 <div>1800,00</div>
               </div>
+            </div>
+            <div>
+              <Typography variant="subtitle1" component="div">
+                Selected: {selectedValue}
+              </Typography>
+              <br />
+              <Teste
+                selectedValue={selectedValue}
+                open={open}
+                onClose={handleClose}
+              />
             </div>
           </div>
         </span>
