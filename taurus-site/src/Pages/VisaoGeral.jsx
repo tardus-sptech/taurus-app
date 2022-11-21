cd5f2fc321e6e5d924956089571acfea5b58fd9c
LucasOs4kwe <LUCAS.SSANTOS@bandtec.com.br>
2022-11-20 23:47:23 -0300

    feat: added spentlist endpoint

--- taurus-site/src/Pages/VisaoGeral.jsx
+++ taurus-site/src/Pages/VisaoGeral.jsx
@@ -5,6 +5,7 @@ import NavBar from "../Components/NavBar";
 import ReactDOM from 'react-dom/client';
 import { StyledEngineProvider } from '@mui/material/styles';
 import BasicModal from "../Components/ModalLancamento";
+import HighSpents from "../Components/HighSpents";
 import api from "../api";
 
 function VisaoGeral() {
@@ -19,16 +20,24 @@ function VisaoGeral() {
 
     const [name, setName] = useState('');
     const [balance, setBalance] = useState('');
+    const [spents, setSpents] = useState([]);
 
     const USER_URL = '/users';
     const idUser = sessionStorage.getItem('id');
 
     const userData = async (e) => {
-
         try {
             const response = await api.get(`${USER_URL}/${idUser}`);
             setName(response.data.name);
             setBalance(Number(response.data.valueInAccount).toFixed(2));
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    const listSpents = async (e) => {
+        try {
+            
         } catch (error) {
             console.error(error);
         }
@@ -78,57 +87,22 @@ function VisaoGeral() {
 
 
                 <div id="second-container">
-                    <div id="left-div">
-                        <div className="my-accounts">
-                            <h1 className="title-container">Minhas contas</h1>
-                            <div id="accounts-list">
-                                <div className="account-user">
-                                    <img src="./img/nubank-logo.png" alt="" className="bank-image-account" />
-                                    <span>
-                                        <span id="account-name">Renan Oliveira</span>
-                                        <span id="type-account">Conta Corrente</span>
-                                    </span>
-                                    <span id="account-balance">R$ 500,00</span>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="credit-cards">
-                            <h1 className="title-container">Cartões de crédito</h1>
-                            <div id="credit-card-list">
-
-                                <div className="credit-card-user">
-                                    <div className="credit-card-header">
-                                        <img src="./img/nubank-logo.png" alt="" className="bank-image-account" />
-                                        <span>
-                                            <span className="bank-name">Nubank</span>
-                                            <span>Cartão de crédito</span>
-                                        </span>
-                                    </div>
-                                    <div className="credit-card-footer">
-                                        <span>
-                                            <span>Disponível</span>
-                                            <span className="credit-avaliable">R$ 2.350,00</span>
-                                        </span>
-                                        <span>
-                                            <span>Fatura</span>
-                                            <span className="credit-bill">R$ 0,00</span>
-                                        </span>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-                    <div id="right-div">
-                        <div className="sign-details">
-                            <h1 id="sign-details"></h1>
-                        </div>
-                        <div className="mouth-balance"></div>
-                        <div className="high-spents"></div>
-                        <div className="month-limits"></div>
+                    <div className="high-spents">
+                            {/* {
+                                spents.map((spents, index) => {
+                                    return (
+                                        <HighSpents 
+                                            key={spents.id}
+                                            spent={spents.name}
+                                            value={spents.value}
+                                            date={spents.date}
+                                        />
+                                    )
+                                })
+                            } */}
+                        <HighSpents />
                     </div>
                 </div>
-
             </main>
 
             <Footer />
