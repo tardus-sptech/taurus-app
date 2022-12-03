d3d136f504ad5f0bd3e9718e069d77ba1d4298af
Victor Linhati <VICTOR.LINHATI@bandtec.com.br>
2022-12-03 00:32:21 -0300

    Update

--- taurus-site/src/Pages/VisualizarUsuario.jsx
+++ taurus-site/src/Pages/VisualizarUsuario.jsx
@@ -1,41 +1,47 @@
-import { TextField } from "@mui/material";
-import React, {useEffect, useState} from "react";
+import { Grid, TextField } from "@mui/material";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Footer from "../Components/Footer";
 import NavBar from "../Components/NavBar";
 import api from "../api";
 
 function VisualizarUsuario() {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
 
-  const USER_URL = '/users';
-  const idUser = sessionStorage.getItem('id');
+  const USER_URL = "/users";
+  const idUser = sessionStorage.getItem("id");
 
-  useEffect( () => {userData()}, []);
+  useEffect(() => {
+    userData();
+  }, []);
 
   const userData = async (e) => {
-      try {
-          const response = await api.get(`${USER_URL}/${idUser}`);
-          setName(response.data.name);
-          setEmail(response.data.email);
-      } catch (error) {
-          console.error(error);
-      }
-  }
+    try {
+      const response = await api.get(`${USER_URL}/${idUser}`);
+      setName(response.data.name);
+      setEmail(response.data.email);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <>
       <NavBar />
       <main id="user-content">
-        <div id="user-container">
-          <TextField disabled label="Nome">
-            {name}
-          </TextField>
-          <TextField disabled label="Email">
-            {email}
-          </TextField>
-        </div>
+        <Grid container id="user-container">
+          <Grid item lg={6}>
+            <TextField label="Nome" color="secondary">
+              {name}
+            </TextField>
+          </Grid>
+          <Grid item lg={6}>
+            <TextField color="secondary" label="Email">
+              {email}
+            </TextField>
+          </Grid>
+        </Grid>
       </main>
       <Footer />
     </>
