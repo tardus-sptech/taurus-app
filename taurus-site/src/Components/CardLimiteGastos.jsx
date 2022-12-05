4433ec6b0bd7baf87fe568729c80409c9752b464
Victor Linhati <VICTOR.LINHATI@bandtec.com.br>
2022-12-04 22:18:58 -0300

    Update

--- taurus-site/src/Components/CardLimiteGastos.jsx
+++ taurus-site/src/Components/CardLimiteGastos.jsx
@@ -2,44 +2,45 @@ import BarraLimite from "./BarraLimite";
 import api from "../api";
 import { useEffect, useState } from "react";
 
-function CardLimiteGastos(props){
-  const id = sessionStorage.getItem('id');
-    const [bars, setBars] = useState([]);
-        useEffect(() => {
-          api.get(`/limities/user/${id}`).then((resposta) => {
+function CardLimiteGastos(props) {
+  const id = sessionStorage.getItem("id");
+  const [bars, setBars] = useState([]);
+  useEffect(() => {
+    api.get(`/limities/user/${id}`).then((resposta) => {
+      setBars(resposta.data);
+      console.log(resposta.data);
+    });
+  }, []);
 
-          setBars(resposta.data);
-         console.log(resposta.data)
-          })
-        }, [])
-
-    return(
-        <>
-              <div className="corFundo">
+  return (
+    <>
+      <div className="corFundo">
         <div className="size-limite">
           <div className="card-limite">
             <div className="limite-boxes">
               <div className="box-limite">
                 <div className="titulo-limite">
                   <h3>Limite de Gastos</h3>
-                  <h4>{props.mes} {props.ano}</h4>
+                  <h4>
+                    {props.mes} {props.ano}
+                  </h4>
                 </div>
                 <div className="limites">
-                {bars.map((item) => (
+                  {bars.map((item) => (
                     <BarraLimite
-                    categoria={item.category.description}
-                    gasto={item.categorySpent}
-                        total={200}
+                      categoria={item.category.description}
+                      gasto={item.categorySpent}
+                      total={200}
                     />
-                    ))}
+                  ))}
                 </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-        </>
-    )
+    </>
+  );
 }
 
 export default CardLimiteGastos;
--- taurus-site/src/Pages/LimiteGastos.jsx
+++ taurus-site/src/Pages/LimiteGastos.jsx
@@ -3,39 +3,23 @@ import React from "react";
 import { useEffect, useState } from "react";
 import NavBar from "../Components/NavBar";
 import Footer from "../Components/Footer";
-import Card from "../Components/CardLimiteGastos"
-import Carousel from "../Components/Carousel";
+import Card from "../Components/CardLimiteGastos";
 
 import api from "../api";
 
 function LimiteGastos() {
-
   const [cards, setCards] = useState([]);
   useEffect(() => {
-          api.get("/limities/").then((resposta) => {
-            setCards(resposta.data);
-            console.log(resposta.data);
-          })
-        }, [])
+    api.get("/limities/").then((resposta) => {
+      setCards(resposta.data);
+      console.log(resposta.data);
+    });
+  }, []);
 
   return (
     <>
       <NavBar />
-      <Carousel
-        card={
-          cards.map((item) => (
-            <Card
-              mes={item.month}
-              ano={2022}
-            />
-        ))
-
-      
-      }
-      >
-
-      </Carousel>
-      
+      <Card />
       <Footer />
     </>
   );
