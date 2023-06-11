f687ed8058004929e98e1d14dd8269d6053f0e71
LucasOs4kwe <lucas@vecta.com.br>
2023-06-11 18:34:31 -0300

    feat: added entry spent screen

---
+++ AppTaurus2/app/src/main/java/com/taurus/apptaurus/LancamentoSpent.kt
@@ -0,0 +1,11 @@
+package com.taurus.apptaurus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LancamentoSpent : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lancamento_spent)
+    }
+}
