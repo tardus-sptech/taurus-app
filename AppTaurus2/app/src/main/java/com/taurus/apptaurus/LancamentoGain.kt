3af11945262f5383dae99c5673930a49dd31e412
LucasOs4kwe <lucas@vecta.com.br>
2023-06-11 06:27:58 -0300

    feat: added entry screen on app

---
+++ AppTaurus2/app/src/main/java/com/taurus/apptaurus/LancamentoGain.kt
@@ -0,0 +1,11 @@
+package com.taurus.apptaurus
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class LancamentoGain : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_lancamento_gain)
+    }
+}
