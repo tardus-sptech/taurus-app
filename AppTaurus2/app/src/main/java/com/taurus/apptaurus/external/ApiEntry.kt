e06da1c325338781ee94f7ccca33444bbf54a6a9
LucasOs4kwe <lucas@vecta.com.br>
2023-06-12 15:53:07 -0300

    fix: fixed earning send endpoint

--- AppTaurus2/app/src/main/java/com/taurus/apptaurus/external/ApiEntry.kt
+++ AppTaurus2/app/src/main/java/com/taurus/apptaurus/external/ApiEntry.kt
@@ -2,6 +2,8 @@ package com.taurus.apptaurus.external
 
 import com.taurus.apptaurus.request.Gain
 import com.taurus.apptaurus.request.GainRequest
+import com.taurus.apptaurus.request.Spent
+import com.taurus.apptaurus.request.SpentRequest
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.Headers
@@ -10,7 +12,11 @@ import retrofit2.http.POST
 interface ApiEntry {
 
     @Headers("Content-Type: application/json")
-    @POST("/api/spenties")
+    @POST("/api/gains")
     fun GainResponse(@Body gainRequest: GainRequest): Call<Gain>
+
+    @Headers("Content-Type: application/json")
+    @POST("/api/spenties")
+    fun SpentResponse(@Body spentRequest: SpentRequest): Call<Spent>
 
 }
